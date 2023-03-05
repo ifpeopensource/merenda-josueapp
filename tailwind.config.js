f2c25ef609d6c47d731507d6b2ee481d04ffc932
Welton Felix <contato.weltonf@gmail.com>
2023-03-05 00:51:37 -0300

    feat: configure tailwind theme, fonts and html metadata
    Load Roboto font, customize tailwind theme, add favicon and change HTML language

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          800: '#2F9E41',
+          900: '#3C8748',
+        },
+      },
+    },
+    fontFamily: {
+      sans: ['Roboto', 'Helvetica', 'Ubuntu', 'Arial', 'sans-serif'],
+    },
   },
   plugins: [],
 };
