2be7ccec7ed6cc008ab6358efaa31b43414a8969
Welton Felix <contato.weltonf@gmail.com>
2023-03-05 13:25:20 -0300

    feat(header): sticky header
    Add position fixed to keep header visible on page scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,19 @@ import logomark from '../assets/josueapp-100.webp';
 
 export function Header() {
   return (
-    <header className="w-full bg-primary-900 h-14 lg:h-16 flex items-center justify-center gap-1">
-      <img
-        src={logomark}
-        alt="JosueApp"
-        className="h-9 lg:h-11"
-        draggable={false}
-      />
-      <span className="block text-neutral-50 text-lg lg:text-xl font-bold">
-        JosueApp
-      </span>
-    </header>
+    <>
+      <div className="h-14 lg:h-16" />
+      <header className="fixed top-0 w-full bg-primary-900 h-14 lg:h-16 flex items-center justify-center gap-1">
+        <img
+          src={logomark}
+          alt="JosueApp"
+          className="h-9 lg:h-11"
+          draggable={false}
+        />
+        <span className="block text-neutral-50 text-lg lg:text-xl font-bold">
+          JosueApp
+        </span>
+      </header>
+    </>
   );
 }
