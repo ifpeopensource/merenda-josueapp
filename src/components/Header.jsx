e2618cb0454ac070e0a7db249e2ae934f8ac9a92
Welton Felix <contato.weltonf@gmail.com>
2023-03-06 14:31:52 -0300

    feat(header): update header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ export function Header() {
     <>
       <div className="h-14 lg:h-16" />
       <header className="fixed top-0 w-full bg-primary-900 h-14 lg:h-16 flex items-center justify-center gap-1">
+        <span className="block text-neutral-50 text-xl lg:text-xl font-bold">
+          JosueApp
+        </span>
         <img
           src={logomark}
           alt="JosueApp"
           className="h-9 lg:h-11"
           draggable={false}
         />
-        <span className="block text-neutral-50 text-lg lg:text-xl font-bold">
-          JosueApp
-        </span>
       </header>
     </>
   );
